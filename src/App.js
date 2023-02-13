3d06f6e6240d102d0151c4f64311f4aa8c4b9767
Samaze123456 <he202053@students.ephec.be>
2023-02-13 16:28:14 +0100

    test of server node.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import { Navbar } from "./components";
 import { LoginPage, HomePage, MarketPage, WheelPage } from "./pages";
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
+<<<<<<< HEAD
   let isConnected = true;
 
+=======
+
+  useEffect(() => {
+    fetch("http://localhost:8000/marmiton")
+      .then((res) => res.json())
+      .then((data) => console.log(data.json));
+  }, []);
+>>>>>>> d5d2d6a (test of server node.js)
   return (
     <div className="App">
       <BrowserRouter>
