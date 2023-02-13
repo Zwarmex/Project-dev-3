b0239d96735bcb843e7f1f73efd343d9ce6bb7bb
Samaze123456 <he202053@students.ephec.be>
2023-02-13 16:33:51 +0100

    resolve issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Navbar } from "./components";
 import { LoginPage, HomePage, MarketPage, WheelPage } from "./pages";
 import { LoginPage, HomePage, MarketPage, WheelPage } from "./pages";
