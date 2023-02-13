fc58baf84a711a68fbee2bcfc19f231b2d7f831b
Samaze123456 <he202053@students.ephec.be>
2023-02-13 21:47:45 +0100

    creation of db and beginning of homepage

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+  <div>
+    <p>
+      Bienvenue dans notre communauté culinaire en ligne ! 
+      Nous vous offrons un moyen facile et amusant de découvrir de nouvelles recettes, 
+      de planifier vos repas et de partager vos créations culinaires avec d'autres foodies. 
+      Inscrivez-vous dès maintenant pour accéder à une bibliothèque de recettes, pour ajouter vos propres recettes préférées à vos favoris, et pour connecter avec d'autres amateurs de cuisine du monde entier. 
+      Commençons à cuisiner !
+    </p>
+  </div>);
 };
 
 export default Header;
