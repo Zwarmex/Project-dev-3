8ef4513f03425aa95904dbfab2ee79206e828f1e
Samaze123456 <he202053@students.ephec.be>
2023-02-13 16:44:36 +0100

    resolved criticals issues with git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
 import { Navbar } from "./components";
 import { LoginPage, HomePage, MarketPage, WheelPage } from "./pages";
-import { LoginPage, HomePage, MarketPage, WheelPage } from "./pages";
 import "./App.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
+  const isConnected = true;
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar isConnected={isConnected} />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/marketplace" element={<MarketPage />} />
-          <Route path="/wheel" element={<WheelPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
       <BrowserRouter>
         <Navbar isConnected={isConnected} />
         <Routes>
