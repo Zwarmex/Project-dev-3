24621182dd3fa9ab2bd1f89f4758b8c3034a6f19
Julien Pollart <jpollart@gmail.com>
2023-02-16 10:18:17 +0100

    [WIP] navbar-menu

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as LoadingSpinner } from "./loadingspinner/LoadingSpinner";
-export {default as RecipeItem} from "./recipeitem/RecipeItem";
+export { default as RecipeItem } from "./recipeitem/RecipeItem";
+export { default as NavbarMenu } from "./navbarmenu/NavbarMenu";
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,80 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import login from "../../assets/login.png";
 import { NavLink } from "react-router-dom";
+import { NavbarMenu } from "../";
 
 const Navbar = ({ isConnected }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const handleClickMenu = () => {
-    alert("clicked menu");
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <div className="navbar-container">
-      <div className="navbar-title">
-        <p>
-          <NavLink to="/">
-            Recipe's App
-          </NavLink>
-        </p>
-      </div>
-      <div className="navbar-links">
-        <ul className="navbar-links__ul">
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? "activeLink" : undefined
-              }
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/marketplace"
-              className={({ isActive }) =>
-                isActive ? "activeLink" : undefined
-              }
-            >
-              Market Place
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/wheel"
-              className={({ isActive }) =>
-                isActive ? "activeLink" : undefined
-              }
-            >
-              Recipe's wheel
-            </NavLink>
-          </li>
-          {isConnected ? (
+    <>
+      <div className="navbar-container">
+        <div className="navbar-title">
+          <p>
+            <NavLink to="/">Recipe's App</NavLink>
+          </p>
+        </div>
+        <div className="navbar-links">
+          <ul className="navbar-links__ul">
             <li>
               <NavLink
-                to="/calendar"
+                to="/"
                 className={({ isActive }) =>
                   isActive ? "activeLink" : undefined
                 }
               >
-                Calendar
+                Home
               </NavLink>
             </li>
-          ) : (
-            null
-          )}
-          <li className="navbar-links__li">
-            {isConnected ? (
-              <img className="navbar-links__li-img" src={login} alt="login" onClick={() => handleClickMenu()} />
-            ) : (
-              <NavLink to="/login">
-                <img className="navbar-links__li-img" src={login} alt="login"/>
+            <li>
+              <NavLink
+                to="/marketplace"
+                className={({ isActive }) =>
+                  isActive ? "activeLink" : undefined
+                }
+              >
+                Market Place
               </NavLink>
-            )}
-          </li>
-        </ul>
+            </li>
+            <li>
+              <NavLink
+                to="/wheel"
+                className={({ isActive }) =>
+                  isActive ? "activeLink" : undefined
+                }
+              >
+                Recipe's wheel
+              </NavLink>
+            </li>
+            {isConnected ? (
+              <li>
+                <NavLink
+                  to="/calendar"
+                  className={({ isActive }) =>
+                    isActive ? "activeLink" : undefined
+                  }
+                >
+                  Calendar
+                </NavLink>
+              </li>
+            ) : null}
+            <li className="navbar-links__li">
+              {isConnected ? (
+                <img
+                  className="navbar-links__li-img"
+                  src={login}
+                  alt="login"
+                  onClick={() => handleClickMenu()}
+                />
+              ) : (
+                <NavLink to="/login">
+                  <img
+                    className="navbar-links__li-img"
+                    src={login}
+                    alt="login"
+                  />
+                </NavLink>
+              )}
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      {menuOpen ? (
+        <div className="navbar__menu">
+          <NavbarMenu />
+        </div>
+      ) : undefined}
+    </>
   );
 };
 
