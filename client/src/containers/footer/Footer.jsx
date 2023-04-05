18fcd7f7d5410674a0af09d167356b200bfc5b29
Julien Pollart <jpollart@gmail.com>
2023-04-05 16:13:43 +0200

    [WIP] Front-end of the app

--- client/src/containers/footer/Footer.jsx
+++ client/src/containers/footer/Footer.jsx
@@ -4,21 +4,31 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="banner">
-        <p>Mail : test@test.com</p>
-        <p>Tel : +32478956895</p>
-      </div>
-      <div className="actionButton">
-        <div className="contactus">
-          <NavLink to="/contact-us">
-            <button>Contactez nous</button>
-          </NavLink>
+    <div className="footer-container">
+      <div className="footer">
+        <div className="banner">
+          <p>
+            Mail :{" "}
+            <a href="mailto:recipesapp-admin@gmail.com">
+              recipesapp-admin@gmail.com
+            </a>{" "}
+          </p>
+          <p>
+            Github :{" "}
+            <a href="https://github.com/Zwarmex/Project-dev-3">Projet-dev-3</a>
+          </p>
         </div>
-        <div className="aboutus">
-          <NavLink to="/about-us">
-            <button>Qui sommes-nous ?</button>
-          </NavLink>
+        <div className="actionButton">
+          <div className="contactus">
+            <NavLink to="/contact-us">
+              <button className="contact-us">Contactez nous</button>
+            </NavLink>
+          </div>
+          <div className="aboutus">
+            <NavLink to="/about-us">
+              <button className="about-us">Qui sommes-nous ?</button>
+            </NavLink>
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/homepage/HomePage.jsx
+++ client/src/pages/homepage/HomePage.jsx
@@ -7,6 +7,28 @@ const HomePage = () => {
   return (
     <div>
       <Header />
+      <div className="HomePage-Content">
+        <div className="nosServices">
+          <p>
+            Nous vous aidons à trouver une recette facile à faire chez vous
+            parmis vos recettes préférées.
+          </p>
+          <p>
+            Une roue vous permettra de tirer une recette au sort. Vous aurez
+            aussi la possiblités d'organiser les repas de la semaine à l'aide du
+            calendrier.
+          </p>
+          <p>
+            Une notification vous parviendra avec la liste de tous les
+            ingrédients à acheter afin de réaliser la recette au mieux.
+          </p>
+          <p>
+            Chaque recette est accompagnées d'une liste d'instructions
+            détaillées afin d'être accompagné tout au long de la réalisation de
+            celle-ci.
+          </p>
+        </div>
+      </div>
       <Footer />
     </div>
   );
