40adbb4335a15175122ca478eb23fc5b402d92b5
Zwarmex <jpollart@gmail.com>
2023-05-31 15:45:26 +0200

    formatting front-end branch

--- server/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const config = {
-	user: process.env.USER,
-	password: process.env.PASSWORD,
-	server: process.env.SERVER,
-	database: process.env.DATABASE,
-	trustServerCertificate: true,
-};
-module.exports = config;
--- server/hashPassword.js
+++
@@ -1,5 +0,0 @@
-const bcrypt = require('bcryptjs');
-module.exports = async function hashPassword(password, salt) {
-	// Hash the password using the provided salt.
-	return await bcrypt.hash(password, salt);
-};
